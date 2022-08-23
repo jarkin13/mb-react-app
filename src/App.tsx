b177798f504e501a5b2e06b0e275e8303456bf02
micblyth <110206029+micblyth@users.noreply.github.com>
2022-08-23 18:42:15 -0500

    add Tag components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import Header from "./ui/Header";
+import Tag from "./ui/Tag";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header /> React Application
+      <Header />
+      <div className="filterbar">
+        <Tag text="All" />
+        <Tag text="Live" />
+        <Tag text="Music" />
+        <Tag text="Gaming" />
+        <Tag text="KSI" />
+      </div>
     </div>
   );
 }
