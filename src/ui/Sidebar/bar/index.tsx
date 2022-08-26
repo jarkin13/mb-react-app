f6b153919d43d1a75bb0e3c86a2bebfb3bf3cb6d
micblyth <110206029+micblyth@users.noreply.github.com>
2022-08-25 20:24:42 -0500

    Added Icon imports to bar

---
+++ src/ui/Sidebar/bar/index.tsx
@@ -0,0 +1,10 @@
+import { ReactComponent as exploreIcon } from "./icon/explore.svg";
+import { ReactComponent as historyIcon } from "./icon/history.svg";
+import { ReactComponent as libraryIcon } from "./icon/library.svg";
+import { ReactComponent as homeIcon } from "./icon/home.svg";
+import { ReactComponent as likeVideosIcon } from "./icon/like-videos.svg";
+import { ReactComponent as shortsIcon } from "./icon/shorts.svg";
+import { ReactComponent as showMoreIcon } from "./icon/show-more.svg";
+import { ReactComponent as subscriptionsIcon } from "./icon/subscriptions.svg";
+import { ReactComponent as watchLaterIcon } from "./icon/watch-later.svg";
+import { ReactComponent as yourVideosIcon } from "./icon/your-videos.svg";
