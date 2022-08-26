d4e66aa10c403b86b98355d0890c7e1c5d0a08c7
micblyth <110206029+micblyth@users.noreply.github.com>
2022-08-25 21:19:02 -0500

    Added css to the sidebar

--- src/ui/Header/Sidebar/index.tsx
+++ src/ui/Header/Sidebar/index.tsx
@@ -13,54 +13,56 @@ import "./index.css";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-link home">
-        <div className="icon home-icon">
-          <HomeIcon />
+      <div className="sidebar-inner">
+        <div className="sidebar-link home">
+          <div className="icon home-icon">
+            <HomeIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link explore">
-        <div className="icon explore-icon">
-          <ExploreIcon />
+        <div className="sidebar-link explore">
+          <div className="icon explore-icon">
+            <ExploreIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link shorts">
-        <div className="icon shorts-icon">
-          <ShortsIcon />
+        <div className="sidebar-link shorts">
+          <div className="icon shorts-icon">
+            <ShortsIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link subscriptions">
-        <div className="icon subs-icon">
-          <SubscriptionsIcon />
+        <div className="sidebar-link subscriptions">
+          <div className="icon subs-icon">
+            <SubscriptionsIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link library">
-        <div className="icon library-icon">
-          <LibraryIcon />
+        <div className="sidebar-link library">
+          <div className="icon library-icon">
+            <LibraryIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link history">
-        <div className="icon history-icon">
-          <HistoryIcon />
+        <div className="sidebar-link history">
+          <div className="icon history-icon">
+            <HistoryIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link your-videos">
-        <div className="icon your-videos-icon">
-          <YourVideosIcon />
+        <div className="sidebar-link your-videos">
+          <div className="icon your-videos-icon">
+            <YourVideosIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link watch-later">
-        <div className="icon watch-later-icon">
-          <WatchLaterIcon />
+        <div className="sidebar-link watch-later">
+          <div className="icon watch-later-icon">
+            <WatchLaterIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link liked-videos">
-        <div className="icon liked-videos-icon">
-          <LikeVideosIcon />
+        <div className="sidebar-link liked-videos">
+          <div className="icon liked-videos-icon">
+            <LikeVideosIcon />
+          </div>
         </div>
-      </div>
-      <div className="sidebar-link show-more">
-        <div className="icon show-more-icon">
-          <ShowMoreIcon />
+        <div className="sidebar-link show-more">
+          <div className="icon show-more-icon">
+            <ShowMoreIcon />
+          </div>
         </div>
       </div>
     </div>
