a21c1d468aebcb5133b0e8984edf858724eb4d4d
micblyth <110206029+micblyth@users.noreply.github.com>
2022-10-03 18:54:28 -0500

    Switched tabs to a single usestate

--- src/ui/Sidebar/index.tsx
+++ src/ui/Sidebar/index.tsx
@@ -23,284 +23,115 @@ import SidebarTab from "./SidebarTab";
 import "../Header/index.css";
 
 function Sidebar() {
-  const [homeActive, setHomeActive] = useState(true);
-  const [historyActive, setHistoryActive] = useState(false);
-  const [exploreActive, setExploreActive] = useState(false);
-  const [libraryActive, setLibraryActive] = useState(false);
-  const [likedvideosActive, setLikedVideosActive] = useState(false);
-  const [shortsActive, setShortsActive] = useState(false);
-  const [showmoreActive, setShowMoreActive] = useState(false);
-  const [watchlaterActive, setWatchLaterActive] = useState(false);
-  const [subscriptionsActive, setSubscriptionsActive] = useState(false);
-  const [home, setHome] = useState(() => <ActiveHomeIcon />);
-  const [history, setHistory] = useState(() => <HistoryIcon />);
-  const [explore, setExplore] = useState(() => <ExploreIcon />);
-  const [library, setLibrary] = useState(() => <LibraryIcon />);
-  const [likedvideos, setLikedVideos] = useState(() => <LikedVideosIcon />);
-  const [shorts, setShorts] = useState(() => <ShortsIcon />);
-  const [showmore, setShowMore] = useState(() => <ShowMoreIcon />);
-  const [subscriptions, setSubscriptions] = useState(() => (
-    <SubscriptionsIcon />
-  ));
-  const [watchlater, setWatchLater] = useState(() => <WatchLaterIcon />);
+  const [activeTab, setActiveTab] = useState("home");
 
-  function SetHomeTab() {
-    setHome(() => <ActiveHomeIcon />);
-    setActiveStateHome();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetExploreTab() {
-    setExplore(() => <ActiveExploreIcon />);
-    setActiveStateExplore();
-    setHome(() => <HomeIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetShortsTab() {
-    setShorts(() => <ActiveShortsIcon />);
-    setActiveStateShorts();
-    setExplore(() => <ExploreIcon />);
-    setHome(() => <HomeIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetSubscriptionsTab() {
-    setSubscriptions(() => <ActiveSubscriptionsIcon />);
-    setActiveStateSubscriptions();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setHome(() => <HomeIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetLibraryTab() {
-    setLibrary(() => <ActiveLibraryIcon />);
-    setActiveStateLibrary();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setHome(() => <HomeIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetHistoryTab() {
-    setHistory(() => <ActiveHistoryIcon />);
-    setActiveStateHistory();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHome(() => <HomeIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetWatchLaterTab() {
-    setWatchLater(() => <ActiveWatchLaterIcon />);
-    setActiveStateWatchLater();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setHome(() => <HomeIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetLikedVideosTab() {
-    setLikedVideos(() => <ActiveLikedVideosIcon />);
-    setActiveStateLikedVideos();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setHome(() => <HomeIcon />);
-    setShowMore(() => <ShowMoreIcon />);
-  }
-  function SetShowMoreTab() {
-    setShowMore(() => <ActiveShowMoreIcon />);
-    setActiveStateShowMore();
-    setExplore(() => <ExploreIcon />);
-    setShorts(() => <ShortsIcon />);
-    setSubscriptions(() => <SubscriptionsIcon />);
-    setLibrary(() => <LibraryIcon />);
-    setHistory(() => <HistoryIcon />);
-    setWatchLater(() => <WatchLaterIcon />);
-    setLikedVideos(() => <LikedVideosIcon />);
-    setHome(() => <HomeIcon />);
+  function handleClick(tab: string) {
+    setActiveTab(tab);
   }
 
-  function setActiveStateHome() {
-    setHomeActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateExplore() {
-    setExploreActive(true);
-    setHomeActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateShorts() {
-    setShortsActive(true);
-    setExploreActive(false);
-    setHomeActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateSubscriptions() {
-    setSubscriptionsActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setHomeActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateLibrary() {
-    setLibraryActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setHomeActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateHistory() {
-    setHistoryActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHomeActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateWatchLater() {
-    setWatchLaterActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setHomeActive(false);
-    setLikedVideosActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateLikedVideos() {
-    setLikedVideosActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setHomeActive(false);
-    setShowMoreActive(false);
-  }
-  function setActiveStateShowMore() {
-    setShowMoreActive(true);
-    setExploreActive(false);
-    setShortsActive(false);
-    setSubscriptionsActive(false);
-    setLibraryActive(false);
-    setHistoryActive(false);
-    setWatchLaterActive(false);
-    setLikedVideosActive(false);
-    setHomeActive(false);
-  }
   return (
     <div className="SidebarAll">
       <TopTabPanel>
-        <TabsClass onClick={SetHomeTab}>
-          <Icons>{home}</Icons>
-          <SidebarTab text="Home" active={homeActive}></SidebarTab>
+        <TabsClass onClick={() => handleClick("home")}>
+          <Icons>
+            {activeTab === "home" ? <ActiveHomeIcon /> : <HomeIcon />}
+          </Icons>
+          <SidebarTab text="Home" active={activeTab === "home"}></SidebarTab>
         </TabsClass>
-        <TabsClass onClick={SetExploreTab}>
-          <Icons>{explore}</Icons>
-          <SidebarTab text="Explore" active={exploreActive}></SidebarTab>
+        <TabsClass onClick={() => handleClick("explore")}>
+          <Icons>
+            {activeTab === "explore" ? <ActiveExploreIcon /> : <ExploreIcon />}
+          </Icons>
+          <SidebarTab
+            text="Explore"
+            active={activeTab === "explore"}
+          ></SidebarTab>
         </TabsClass>
-        <TabsClass onClick={SetShortsTab}>
-          <Icons>{shorts}</Icons>
-          <SidebarTab text="Shorts" active={shortsActive}></SidebarTab>
+        <TabsClass onClick={() => handleClick("shorts")}>
+          <Icons>
+            {activeTab === "shorts" ? <ActiveShortsIcon /> : <ShortsIcon />}
+          </Icons>
+          <SidebarTab
+            text="Shorts"
+            active={activeTab === "shorts"}
+          ></SidebarTab>
         </TabsClass>
-        <TabsClass onClick={SetSubscriptionsTab}>
-          <Icons>{subscriptions}</Icons>
+        <TabsClass onClick={() => handleClick("subscriptions")}>
+          <Icons>
+            {activeTab === "subscriptions" ? (
+              <ActiveSubscriptionsIcon />
+            ) : (
+              <SubscriptionsIcon />
+            )}
+          </Icons>
           <SidebarTab
             text="Subscriptions"
-            active={subscriptionsActive}
+            active={activeTab === "subscriptions"}
           ></SidebarTab>
         </TabsClass>
       </TopTabPanel>
       <hr></hr>
-      <TabsClass onClick={SetLibraryTab}>
-        <Icons>{library}</Icons>
-        <SidebarTab text="Library" active={libraryActive}></SidebarTab>
-      </TabsClass>
-      <TabsClass onClick={SetHistoryTab}>
-        <Icons>{history}</Icons>
-        <SidebarTab text="History" active={historyActive}></SidebarTab>
-      </TabsClass>
-      <TabsClass>
+      <TabsClass onClick={() => handleClick("library")}>
         <Icons>
-          <YourVideosIcon />
+          {activeTab === "library" ? <ActiveLibraryIcon /> : <LibraryIcon />}
         </Icons>
-        <Textstyle>Your Videos</Textstyle>
+        <SidebarTab
+          text="Library"
+          active={activeTab === "library"}
+        ></SidebarTab>
       </TabsClass>
-      <TabsClass onClick={SetWatchLaterTab}>
-        <Icons>{watchlater}</Icons>
-        <SidebarTab text="Watch Later" active={watchlaterActive}></SidebarTab>
+      <TabsClass onClick={() => handleClick("history")}>
+        <Icons>
+          {activeTab === "history" ? <ActiveHistoryIcon /> : <HistoryIcon />}
+        </Icons>
+        <SidebarTab
+          text="History"
+          active={activeTab === "history"}
+        ></SidebarTab>
       </TabsClass>
-      <TabsClass onClick={SetLikedVideosTab}>
-        <Icons>{likedvideos}</Icons>
-        <SidebarTab text="Liked Videos" active={likedvideosActive}></SidebarTab>
+      <TabsClass onClick={() => handleClick("yourVideos")}>
+        <Icons>
+          {activeTab === "yourVideos" ? <YourVideosIcon /> : <YourVideosIcon />}
+        </Icons>
+        <SidebarTab
+          text="Your Videos"
+          active={activeTab === "yourVideos"}
+        ></SidebarTab>
       </TabsClass>
-      <TabsClass onClick={SetShowMoreTab}>
-        <Icons>{showmore}</Icons>
-        <SidebarTab text="Show More" active={showmoreActive}></SidebarTab>
+      <TabsClass onClick={() => handleClick("watchLater")}>
+        <Icons>
+          {activeTab === "watchLater" ? (
+            <ActiveWatchLaterIcon />
+          ) : (
+            <WatchLaterIcon />
+          )}
+        </Icons>
+        <SidebarTab
+          text="Watch Later"
+          active={activeTab === "watchLater"}
+        ></SidebarTab>
+      </TabsClass>
+      <TabsClass onClick={() => handleClick("likedVideos")}>
+        <Icons>
+          {activeTab === "likedVideos" ? (
+            <ActiveLikedVideosIcon />
+          ) : (
+            <LikedVideosIcon />
+          )}
+        </Icons>
+        <SidebarTab
+          text="Liked Videos"
+          active={activeTab === "likedVideos"}
+        ></SidebarTab>
+      </TabsClass>
+      <TabsClass onClick={() => handleClick("showMore")}>
+        <Icons>
+          {activeTab === "showMore" ? <ActiveShowMoreIcon /> : <ShowMoreIcon />}
+        </Icons>
+        <SidebarTab
+          text="Show More"
+          active={activeTab === "showMore"}
+        ></SidebarTab>
       </TabsClass>
     </div>
   );
