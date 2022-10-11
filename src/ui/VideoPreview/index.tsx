d47f0efed7be8fc2c72629eafb10b35bec8ba869
micblyth <110206029+micblyth@users.noreply.github.com>
2022-10-11 13:53:08 -0500

    Started VideoPreview Component

---
+++ src/ui/VideoPreview/index.tsx
@@ -0,0 +1,10 @@
+import { VideoDescriptionText, VideoPreview, VideoTitleText } from "./subcomponents";
+
+interface VideoPreviewProps {
+  text: string;
+}
+const VideoPreview = ({VideoDescriptionText, VideoPreview, VideoTitleText}) => {
+  return(
+
+  )
+}
---
+++ src/ui/VideoPreview/subcomponents.tsx
@@ -0,0 +1,5 @@
+import styled, { css } from "styled-components/macro";
+
+const VideoDescriptionText = css``;
+const VideoThumbnail = css``;
+const VideoTitleText = css``;
